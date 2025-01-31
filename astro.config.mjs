689574efc5ea59a02c1428bb7f31801b16f99373
Ampersander <romain-b49460@hotmail.com>
2025-01-31 17:41:15 +0100

    Add adapter vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
+import vercel from '@astrojs/vercel';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://nebulacode.com',
   integrations: [react()],
   output: 'server',
+  adapter: vercel(),
 });
